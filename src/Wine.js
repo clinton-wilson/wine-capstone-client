9901f7017ac3493f5af1c003ecc595cfd301a219
Clinton Wilson <wilson.clinton.c@gmail.com>
2022-09-08 19:20:15 -0500

    added login and registration capabilities

---
+++ src/Wine.js
@@ -0,0 +1,9 @@
+import { NavBar } from "./components/nav/NavBar"
+import { ApplicationViews } from "./components/views/ApplicationViews"
+
+export const Wine = () => {
+    return <>
+        <NavBar />
+        <ApplicationViews />
+    </>
+}
---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../auth/Login"
+import { Register } from "../auth/Register"
+import { Authorized } from "./Authorized"
+
+
+
+export const ApplicationViews = () => {
+    return <>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+                
+            </Route>
+        </Routes>
+    </>
+}
